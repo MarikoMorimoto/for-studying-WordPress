29e8f2a00ba6057a49a76888137fc89a3ff93ab2
marikomorimoto <mariko.morimoto@c.tarosky.co.jp>
2023-11-12 16:50:31 +0900

    htmlファイルをphpファイルに置き換え

---
+++ page-company.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+<div class="page-inner">
+  <div class="page-main" id="pg-common">
+    <ul class="commons">
+      <li class="common-item">
+        <a class="common-link" href="#">
+          <div class="common-image">
+            <img src="#" alt="" />
+          </div>
+          <div class="common-body">
+            <p class="name">会社概要</p>
+            <p class="caption">私たちパシフィックモール開発は 世界各地のショッピングモール開発を通じて 人と人...</p>
+            <div class="buttonBox">
+              <button type="button" class="seeDetail">MORE</button>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="common-item">
+        <a class="common-link" href="#">
+          <div class="common-image">
+            <img src="#" alt="" />
+          </div>
+          <div class="common-body">
+            <p class="name">事業紹介</p>
+            <p class="caption">私どもパシフィックモール開発株式会社は、創業より一貫して、東南アジア・太平洋地域...</p>
+            <div class="buttonBox">
+              <button type="button" class="seeDetail">MORE</button>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="common-item">
+        <a class="common-link" href="#">
+          <div class="common-image">
+            <img src="#" alt="" />
+          </div>
+          <div class="common-body">
+            <p class="name">沿革</p>
+            <p class="caption">2000年1月 パシフィックモール開発株式会社を東京都港区に設立、商業施設の開発...</p>
+            <div class="buttonBox">
+              <button type="button" class="seeDetail">MORE</button>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="common-item">
+        <a class="common-link" href="#">
+          <div class="common-image">
+            <img src="#" alt="" />
+          </div>
+          <div class="common-body">
+            <p class="name">アクセス</p>
+            <p class="caption">住所 〒100-0000東京都千代田区大手町0-1-2 パシフィックモールビルデ...</p>
+            <div class="buttonBox">
+              <button type="button" class="seeDetail">MORE</button>
+            </div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+ <?php get_footer(); ?>
