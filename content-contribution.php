3305b9a4e75c20c035824ec6c1f3527be16d9e26
marikomorimoto <mariko.morimoto@c.tarosky.co.jp>
2024-02-08 14:21:00 +0900

    カスタムタクソノミーの一覧ページにて、ACFで設定した画像が表示されるように変更

--- content-contribution.php
+++ content-contribution.php
@@ -1,7 +1,14 @@
 <article class="article-card">
 	<a class="card-link" href="<?php echo get_term_link( $term ); // このファイルは include を使って呼び出されルことを前提として作成している。そのため呼び出し元で定義された変数 $term を参照する ?>">
 		<div class="image">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png">
+            <?php
+            if ( function_exists( 'get_field') ) :
+                $image_id = get_field( 'event_image', $term->taxonomy . '_' . $term->term_id );
+                echo wp_get_attachment_image( $image_id, 'contribution' );
+            else :
+                ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png">
+            <?php endif; ?>
 		</div>
 		<div class="body">
 			<time><?php the_time( 'Y.m.d' ); ?></time>
