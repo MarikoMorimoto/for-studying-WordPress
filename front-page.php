130e6ed153f113fe1f7b04898076fca80a61d53e
marikomorimoto <mariko.morimoto@c.tarosky.co.jp>
2024-02-08 17:34:46 +0900

    様々なタイプのページで、それぞれACFで登録したコンテンツが表示されるように

--- front-page.php
+++ front-page.php
@@ -49,7 +49,7 @@
 			setup_postdata( $post );
 			$contribution_title = get_the_title();
 			?>
-			<span class="section-title-en">Regional Contribution</span>
+			<span class="section-title-en"><?php if ( function_exists( 'the_field' ) ) { the_field( 'english_title' ); } ?></span>
 			<h2 class="section-title"><?php the_title(); ?></h2>
 			<p class="section-lead"><?php echo get_the_excerpt(); ?></p>
 			<div class="articles">
@@ -90,7 +90,13 @@
 	<section class="section-contents" id="news">
 		<div class="wrapper">
 			<?php $term_obj = get_term_by( 'slug', 'news', 'category' ); // タームのslug,ID,nameなどを指定することでそのタームのオブジェクトを取得する ?>
-			<span class="section-title-en">News Release</span>
+			<span class="section-title-en">
+                <?php
+                if ( function_exists( 'the_field' ) ) :
+                    the_field( 'english_title', $term_obj->taxonomy . '_' . $term_obj->term_id );
+                endif;
+                ?>
+            </span>
 			<h2 class="section-title"><?php echo $term_obj->name; ?></h2>
 			<p class="section-lead"><?php echo $term_obj->description; ?></p>
 			<ul class="news">
@@ -125,7 +131,7 @@
 			$post = get_page_by_path( 'company' );
 			setup_postdata( $post );
 			?>
-			<span class="section-title-en">Corporate Information</span>
+			<span class="section-title-en"><?php if ( function_exists( 'the_field' ) ) { the_field( 'english_title' ); } ?></span>
 			<h2 class="section-title"><?php the_title(); ?></h2>
 			<p class="section-lead"><?php echo get_the_excerpt(); ?></p>
 			<div class="section-buttons">
